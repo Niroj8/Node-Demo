77df4234d41ce01a2a3f86fd658fdc43fe4b5da9
Your Name <you@example.com>
2020-09-28 09:23:17 +0545

    Demo file saved

---
+++ Data_type/index.js
@@ -0,0 +1,19 @@
+let integer_value=1;
+let float_value=2.89;
+let string_value='hello world';
+let bool_value=false;
+let null_value=null;
+let undefined_value;
+const CONSTANT_VALUE ='IIMS';
+
+console.log("=======");
+console.log(integer_value);
+console.log(string_value);
+console.log(bool_value);
+console.log(undefined_value);
+console.log("======");
+
+
+
+
+

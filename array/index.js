740be49a1764ab50eee2f9552a55008af5789259
Your Name <you@example.com>
2020-10-03 21:54:03 +0545

    Array is added

---
+++ array/index.js
@@ -0,0 +1,23 @@
+//let fruit1 = 'apple', fruit2 = 'mango', fruit3 = 'pears';
+//let fruit = ['apple', 'mango', 'pears', 'kiwi'];
+//console.log(fruit1, fruit2, fruit3);
+//console.log(fruit[1])
+
+
+//let newArr = [['hello',2, true], ['iims','bscit'],['kathmandu','pokhara','dharan']];
+//console.log(newArr[2][2]);
+
+// let fruit1 = 'apple', fruit2 = 'mango', fruit3 = 'pears';
+// const varName =[1,2]
+// varName[1] = 'hello'
+// console.log(varName)
+// let fruit = ['apple', 'mango', 'pears', 'kiwi', 1, true, null, varName];
+// fruit[7][1] = 'hello';
+// console.log(fruit);
+
+
+let newArr = [['hello',2, true], ['iims','bscit'],['kathmandu','pokhara','dharan']];
+newArr[2][0] ='Jhapa'
+console.log(newArr);
+
+

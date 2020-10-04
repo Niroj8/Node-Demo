2de9f5e0565f42896c2ff9589d0e08431f205ea5
Your Name <you@example.com>
2020-10-04 09:05:18 +0545

    array is added

--- array/index.js
+++ array/index.js
@@ -7,6 +7,7 @@
 //let newArr = [['hello',2, true], ['iims','bscit'],['kathmandu','pokhara','dharan']];
 //console.log(newArr[2][2]);
 
+
 // let fruit1 = 'apple', fruit2 = 'mango', fruit3 = 'pears';
 // const varName =[1,2]
 // varName[1] = 'hello'
@@ -16,8 +17,36 @@
 // console.log(fruit);
 
 
-let newArr = [['hello',2, true], ['iims','bscit'],['kathmandu','pokhara','dharan']];
-newArr[2][0] ='Jhapa'
-console.log(newArr);
+// let newArr = [['hello',2, true], ['iims','bscit'],['kathmandu','pokhara','dharan']];
+// newArr[2][0] ='Jhapa'
+// console.log(newArr);
 
 
+// add joker at index 2 without removing any items
+// let dc = ['superman', 'batman', ['flash','wonderwoman'], 'aquaman'];
+// dc.splice(2,0, 'joker');
+// console.log(dc);
+
+
+// let dc = ['superman', 'batman', 'aquaman'];
+// dc.splice(3,0,'joker');
+// console.log(dc);
+
+
+// first argument passed on slice will include that element but will remove all elements before it
+// second argument passed on slice method will not include element of that index and removes every elements after that elements including itself
+// let fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// let citrus = fruits.slice(1,2);
+// console.log(fruits);
+// console.log(citrus);
+
+
+// let dc = ['superman', 'batman', 'aquaman', 'flash','wonderwoman'];
+// console.log(dc.slice(3));
+
+let student = {};
+student.name = 'Niroj';
+student.id = 234;
+student.gender = 'male';
+student.faculty = 'IT';
+console.log(student)
